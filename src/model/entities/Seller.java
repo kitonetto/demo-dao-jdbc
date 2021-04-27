dc17504e96d47ac78cbb7fa62f7a681b03e6ea98
kitonetto <baixokito@gmail.com>
2021-04-27 12:35:32 -0300

    findById implementation

--- src/model/entities/Seller.java
+++ src/model/entities/Seller.java
@@ -102,7 +102,8 @@ public class Seller implements Serializable {
 
 	@Override
 	public String toString() {
-		return "Seller: " + name + ", " + email + ", " + birthDate + ", $ " + String.format("%.2f", baseSalary)
-				+ ", department of " + department.getName();
+		return "Seller [id=" + id + ", name=" + name + ", email=" + email + ", birthDate=" + birthDate + ", baseSalary="
+				+ baseSalary + ", department=" + department + "]";
 	}
+
 }
